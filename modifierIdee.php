7146dcdde1747d9d54f67019cba43fce2ddc64c8
firashamila33 <firas.hamila@esprit.tn>
2018-01-13 15:03:57 +0100

    on Edit btn clicked redirection to page edit with native php

--- modifierIdee.php
+++ modifierIdee.php
@@ -8,20 +8,21 @@
 
 include "crudIdee.php";
 
-$Idee=new Idee();
-$Idee->setId($_POST['id']);
-$Idee->setTitre($_POST['titre']);
-$Idee->setDomaine($_POST['domaine']);
-$Idee->setDecription($_POST['description']);
-$Idee->setDateAjout($_POST['dateAjout']);
-$Idee->setPrix($_POST['prix']);
-$Idee->setPathImg($_POST['pathImg']);
-$Idee->setPathDoc($_POST['pathDoc']);
+//$Idee=new Idee();
+//$Idee->setId($_POST['id']);
+//$Idee->setTitre($_POST['titre']);
+//$Idee->setDomaine($_POST['domaine']);
+//$Idee->setDecription($_POST['description']);
+//$Idee->setDateAjout($_POST['dateAjout']);
+//$Idee->setPrix($_POST['prix']);
+//$Idee->setPathImg($_POST['pathImg']);
+//$Idee->setPathDoc($_POST['pathDoc']);
+//
+//$crudIdee=new crudIdee();
+//$crudIdee->modifier($Idee);
+//
+//echo json_encode($Idee);
 
-$crudIdee=new crudIdee();
-
-$crudIdee->modifier($Idee);
-
-echo json_encode($Idee);
+header('location: ../IdeeEdit.php');
 
 ?>
